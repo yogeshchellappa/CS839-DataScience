afd1ea9bdfc61e865bd0049af457469a66c66c7f
Shantanu Singhal <shantanusinghal2709@gmail.com>
2018-02-26 19:26:52 -0600

    added two new features to features.py

--- features.py
+++ features.py
@@ -61,6 +61,70 @@ class Features(object):
         data["isCapitalized"] = data['term'].str.contains('[A-Z]', regex=True)
 		return data
 
+    def extract_features(data, data_orig, path_adj, path_veg):
+        '''
+        Takes the complete dataframe and returns an array of features
+        [prefix_food_desc, contains_veggie_or_fruit]
+        '''
+
+        doc_id = 'docID'
+        term_id = 'term_id'
+        term = 'term'
+
+        p_doc_id, p_term = None, None
+        prefix_food_desc, contains_veggie_or_fruit = [], []
+
+        # read list of words describing food
+        food_adj = pd.read_csv(path_adj)['words'].tolist()
+
+        # read list of vegetable and fruit names
+        veggie_and_fruit = pd.read_csv(path_veg)['words'].tolist()
+
+        # iterate over each row in the dataframe
+        for index, row in data.iterrows():
+            # get index of current row in original dataframe
+            orig_index = data_orig.index[(data_orig[doc_id] == row[doc_id]) & (data_orig[term_id] == row[term_id])][0]
+
+            # get previous and next rows from original
+            if orig_index > 0:
+                print("index")
+                print(orig_index)
+                p_term = data_orig.iloc[[orig_index-1],:][term].values[0]
+                p_doc_id = data_orig.iloc[[orig_index-1],:][doc_id].values[0]
+            
+            # if not the first word in the document and is contained in food adjectives list
+            if p_doc_id and p_doc_id == row[doc_id] and p_term in food_adj:
+                prefix_food_desc.append(1)
+            else:
+                prefix_food_desc.append(0)
+
+            # if any sub-string of the word is a vegetable or fruit name
+            contains_veggie_or_fruit.append(0)
+            for s in getAllSubstrings(row[term]):
+                if s in veggie_and_fruit:
+                    contains_veggie_or_fruit.pop()
+                    contains_veggie_or_fruit.append(1)
+                    break
+
+        data['hasDescriptivePrefix'] = prefix_food_desc
+        data['hasIngredient'] = contains_veggie_or_fruit
+        
+
+    def getAllSubstrings(word):
+        tok = word.strip().split(' ')
+        if len(tok) == 1:
+            return tok
+        else:
+            all_toks = list(tok)
+            for i in range(len(tok)):
+                for offset in range(2, len(tok)):
+                    if i+offset > len(tok):
+                        break
+                    else:
+                        all_toks.append(" ".join(tok[i:i+offset]))
+                        all_toks.append(word)
+                        return all_toks
+
 
 
 '''
